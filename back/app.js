bd3496870ecad37da91aa51aa3ffe9fa1117f7c1
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-05-06 11:09:04 +0200

    adding env infos, creating index route

--- back/app.js
+++ back/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('corrs');
-const router = require('./routes/indexedDB.routes');
+const router = require('./routes/index.routes');
 const port=process.env.PORT || 8000;
 
 //need to create mongo connection
