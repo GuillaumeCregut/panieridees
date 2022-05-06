a3e244c5316dd49e68f505108ff2c270c1d0134e
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-05-06 10:34:05 +0200

    adding back first files

---
+++ back/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('corrs');
+const router = require('./routes/indexedDB.routes');
+const port=process.env.PORT || 8000;
+
+//need to create mongo connection
+
+const corsOptions ={
+    // origin:process.env.CLIENT_URL,  
+    origin: true,
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+  }
+
+const app=express();
+app.use(express.urlencoded({extended:true}));
+app.use(cors(corsOptions));
+app.use('/api',router);
+
+//Prevent response on /
+app.get('/',(req,res)=>{
+    res.send('Welcome');
+});
+
+//Start server
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`)
+});
+
+module.exports = app;
