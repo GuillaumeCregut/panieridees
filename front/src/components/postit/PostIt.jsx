592aba171bdd8a694dae08b40e5b6c3a0ca9dfd6
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-05-08 13:15:34 +0200

    animation OK

--- front/src/components/postit/PostIt.jsx
+++ front/src/components/postit/PostIt.jsx
@@ -9,17 +9,18 @@ const PostIt = ({setSelectedTheme,selectedTheme, textIdea,setTextIdea}) => {
     useEffect(()=>{
         const getTheme= async ()=>{
             const url=`${process.env.REACT_APP_API_URL}themes`;
-            console.log(url);
             await axios
                 .get(url)
                 .then((result)=>{
                     if(result.status===200){
-                        console.log(result.data);
                         setThemeList(result.data)
                     }
                 })
                 .catch((err)=>{
                     console.log(err);
+                    if(err.code==='ERR_NETWORK'){
+                        alert(`L'API est hébergée par Heroku, il faut attendre 15 sec pour son démarrage. Veuillez réessayer dans 15 sec.`)
+                      }
                 })
         }
         getTheme();
@@ -29,9 +30,12 @@ const PostIt = ({setSelectedTheme,selectedTheme, textIdea,setTextIdea}) => {
     <div className='PostIt'>
         <label htmlFor="themeSelect">Thématique souhaitée :
             <select id='themeSelect' value={selectedTheme} onChange={(e)=>setSelectedTheme(e.target.value)}>
+            <option value=''>--</option>
             {themeList.map((theme)=>{
                 return(
-                    <option value={theme._id}> {theme.name} </option>
+                    <option
+                         value={theme._id}
+                         key={theme._id}> {theme.name} </option>
                 )
             })}
             </select>
--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -1,47 +1,110 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import Cart from '../../components/cart/Cart';
-
 import PostIt from '../../components/postit/PostIt';
-
+import bellSound from '../../assets/bell.ogg';
 import './Home.scss';
 
 const Home = () => {
   const [selectedTheme, setSelectedTheme] = useState('');
   const [textIdea, setTextIdea] = useState('');
-  
+  let idPostIt=0;
+
   //draggable events
   function allowDrop(ev){
     ev.preventDefault();
   }
-  const repoIdea=document.getElementById('homeRepo');
-  repoIdea.addEventListener('drop',function (e){
-    drop(e);
-  });
-  repoIdea.addEventListener('dragover',function (e){
-    allowDrop(e);
-  });
+
+
   function drag(ev) {
     ev.dataTransfer.setData("text/plain", ev.target.id);
-    let Id_PostIt = ev.target.id;
-  }
-
-  function drop(ev){
-    let target=0;
-    ev.preventDefault();
-    let data=ev.dataTransfer.getData("text");
-    let targetDiv=ev.target.id;
-    if(targetDiv==='homeRepo'){
-      alert('Dépot');
+    idPostIt = ev.target.id;
+    if(idPostIt!=='HomePostItContainer')
+    {
+      ev.preventDefault();
     }
   }
 
+  function drop(ev){
+    ev.preventDefault();
+    let data=ev.dataTransfer.getData("text");
+    let targetDiv=ev.target.id;
+    if((targetDiv==='homeRepo')&&(idPostIt==='HomePostItContainer') ){
+      ev.target.appendChild(document.getElementById(data));
+      const newIdea={
+        state : false,
+        name:textIdea,
+        theme:selectedTheme
+      }
+      const url=`${process.env.REACT_APP_API_URL}ideas`;
+      axios
+        .post(url,newIdea)
+        .then((result)=>{
+          if(result.status===201){
+            //animate, then restore,play sound
+            playSound();
+          }
+        })
+        .catch((err)=>{
+          if(err.code==='ERR_NETWORK'){
+           // alert(`L'API est hébergée par Heroku, il faut attendre 15 sec pour son démarrage. Veuillez réessayer dans 15 sec.`)
+          }
+          animatePostIt();
+        })
+    }
+  }
+
+  const reattribute=()=>{
+    const leftPanel=document.getElementById('HomeLeft');
+    leftPanel.appendChild(document.getElementById('HomePostItContainer'));
+  }
+
+  const playSound=()=>{
+    let audio = new Audio(bellSound);
+    audio.play();
+  }
+
+//Animation
+  const animatePostIt=()=>{
+    const postItContainer=document.getElementById('HomePostItContainer');
+    const translatePostIt=new KeyframeEffect(
+      postItContainer,
+      [
+        {transform: 'translateY(0%) scale(1)'
+      },
+        {transform : 'translateY(200px) scale(0)'
+      }
+      ],
+      {
+        duration : 1000,
+        iterations:1
+      }
+    );
+    const postItAnim=new Animation(translatePostIt,document.timeline);
+    postItAnim.play();
+    Promise.all(
+      postItContainer
+        .getAnimations().map(
+          (animation)=>{
+            return animation.finished
+          }
+        )
+        ).then(()=>{
+          reattribute();
+        }
+    );
+  }
 
   return (
     <div className='Home'>
       <h2>Accueil</h2>
       <div className='HomeContainer'>
-        <div className="HomeLeft">
-          <div className="HomePostItContainer" draggable="true" id="HomePostItContainer">
+        <div className="HomeLeft" id="HomeLeft">
+          <div
+            className="HomePostItContainer"
+            draggable="true"
+            id="HomePostItContainer"
+            onDragStart={(e)=>drag(e)}>
             <PostIt
               selectedTheme={selectedTheme}
               setSelectedTheme={setSelectedTheme}
@@ -51,10 +114,15 @@ const Home = () => {
           </div>
         </div>
         <div className="HomeRight">
-          <div className="HomeRepo" id="homeRepo">
+          <div
+            className="HomeRepo"
+            id="homeRepo"
+            onDrop={(e)=>drop(e)}
+            onDragOver={(e)=>allowDrop(e)}>
             Déposer votre idée ici
           </div>
           <Cart />
+          <button onClick={reattribute}>Go</button>
         </div>
       </div>
     </div>
