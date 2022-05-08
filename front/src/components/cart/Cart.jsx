ae1bc50f4eea94a544fe39dbf3180cb803b43ec5
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-05-08 17:17:06 +0200

    adding states

--- front/src/components/cart/Cart.jsx
+++ front/src/components/cart/Cart.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import cartsvg from "../../assets/cart.svg";
 import './Cart.scss';
 
 const Cart = () => {
+  const [ideas,setIdeas]=useState([]);
+
   return (
     <div className='Cart'>
         <p><img src={cartsvg} alt="" className='HomeCartIdea'/> </p>
