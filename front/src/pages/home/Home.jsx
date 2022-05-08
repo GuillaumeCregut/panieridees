b63d4302fc34ffc9cea32eaa5bdd2b8b193a845a
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-05-08 10:30:55 +0200

    adding drag and drop

--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Cart from '../../components/cart/Cart';
 
 import PostIt from '../../components/postit/PostIt';
@@ -6,23 +6,52 @@ import PostIt from '../../components/postit/PostIt';
 import './Home.scss';
 
 const Home = () => {
-  const [selectedTheme, setSelectedTheme]=useState('');
-  const [textIdea,setTextIdea]=useState('');
+  const [selectedTheme, setSelectedTheme] = useState('');
+  const [textIdea, setTextIdea] = useState('');
+  
+  //draggable events
+  function allowDrop(ev){
+    ev.preventDefault();
+  }
+  const repoIdea=document.getElementById('homeRepo');
+  repoIdea.addEventListener('drop',function (e){
+    drop(e);
+  });
+  repoIdea.addEventListener('dragover',function (e){
+    allowDrop(e);
+  });
+  function drag(ev) {
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+    let Id_PostIt = ev.target.id;
+  }
+
+  function drop(ev){
+    let target=0;
+    ev.preventDefault();
+    let data=ev.dataTransfer.getData("text");
+    let targetDiv=ev.target.id;
+    if(targetDiv==='homeRepo'){
+      alert('Dépot');
+    }
+  }
+
 
   return (
     <div className='Home'>
       <h2>Accueil</h2>
       <div className='HomeContainer'>
         <div className="HomeLeft">
-          <PostIt 
-            selectedTheme={selectedTheme}
-            setSelectedTheme={setSelectedTheme}
-            textIdea={textIdea}
-            setTextIdea={setTextIdea}
-          />
+          <div className="HomePostItContainer" draggable="true" id="HomePostItContainer">
+            <PostIt
+              selectedTheme={selectedTheme}
+              setSelectedTheme={setSelectedTheme}
+              textIdea={textIdea}
+              setTextIdea={setTextIdea}
+            />
+          </div>
         </div>
         <div className="HomeRight">
-          <div className="HomeRepo">
+          <div className="HomeRepo" id="homeRepo">
             Déposer votre idée ici
           </div>
           <Cart />
